af71357fe6a65fb8ad3021a12c6773e74934a6cf
escalante-cr <sergio.escalante.3@icloud.com>
2026-05-06 17:20:26 -0700

    updated App.vue to fetch backend API instead of localhost

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -13,17 +13,20 @@
 <script setup>
 import { ref, onMounted } from 'vue'
 
+const API = 'https://vue-js-with-flask-backend.onrender.com'
+
 const items = ref([])
+
 const res = await fetch('https://vue-js-with-flask-backend.onrender.com/items')
 
 onMounted(async () => {
-    const res = await fetch('http://localhost:5000/items')
+    const res = await fetch('${API}/items')
     items.value = await res.json()
 })
 
 async function addItem() {
     const newItem = { id: Date.now(), name: 'New item' }
-    await fetch('http://localhost:5000/items', {
+    await fetch('${API}/items', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(newItem)
