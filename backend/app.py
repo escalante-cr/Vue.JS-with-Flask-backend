c013a5a6680dfc4565c35d6f59923c06b165df6d
escalante-cr <sergio.escalante.3@icloud.com>
2026-05-06 17:14:20 -0700

    fix CORS for prod

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,8 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, origins=["http://localhost:5000"])
+CORS(app, origins=["http://localhost:5173",
+     "https://vue-js-with-flask-backend-1.onrender.com/"])
 
 items = [{"id": 1, "name": "Apple"}, {"id":2, "name":"Banana"}]
 
