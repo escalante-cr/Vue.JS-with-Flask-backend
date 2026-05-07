4b90d0165eac9463c57fa7f16a92d2cf48ccd18c
escalante-cr <sergio.escalante.3@icloud.com>
2026-05-06 18:00:45 -0700

    edits to App.vue using single quotes for template is not the same as back quotes

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -17,16 +17,14 @@ const API = 'https://vue-js-with-flask-backend.onrender.com'
 
 const items = ref([])
 
-const res = await fetch('https://vue-js-with-flask-backend.onrender.com/items')
-
 onMounted(async () => {
-    const res = await fetch('${API}/items')
+    const res = await fetch(`${API}/items`)
     items.value = await res.json()
 })
 
 async function addItem() {
     const newItem = { id: Date.now(), name: 'New item' }
-    await fetch('${API}/items', {
+    await fetch(`${API}/items`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(newItem)
